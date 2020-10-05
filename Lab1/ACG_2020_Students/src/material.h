2d68dfd0ec93d71bfdfe82321ed339be80f8187b
u150271 <u150271@54006p12u.ds.upf.edu>
2020-10-05 19:23:18 +0200

    add: framework inicial campus

---
+++ Lab1/ACG_2020_Students/src/material.h
@@ -0,0 +1,41 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+#include "framework.h"
+#include "shader.h"
+#include "camera.h"
+#include "mesh.h"
+
+class Material {
+public:
+
+	Shader* shader = NULL;
+	Texture* texture = NULL;
+	vec4 color;
+
+	virtual void setUniforms(Camera* camera, Matrix44 model) = 0;
+	virtual void render(Mesh* mesh, Matrix44 model, Camera * camera) = 0;
+	virtual void renderInMenu() = 0;
+};
+
+class StandardMaterial : public Material {
+public:
+
+	StandardMaterial();
+	~StandardMaterial();
+
+	void setUniforms(Camera* camera, Matrix44 model);
+	void render(Mesh* mesh, Matrix44 model, Camera * camera);
+	void renderInMenu();
+};
+
+class WireframeMaterial : public StandardMaterial {
+public:
+
+	WireframeMaterial();
+	~WireframeMaterial();
+
+	void render(Mesh* mesh, Matrix44 model, Camera * camera);
+};
+
+#endif
