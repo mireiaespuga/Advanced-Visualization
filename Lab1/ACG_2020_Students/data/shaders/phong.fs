69a929815524a99b064460ff79201f784b97aa75
AinaVendrell <ainavendrell99@gmail.com>
2020-10-17 19:57:07 +0200

    Lab1: improve light

--- Lab1/ACG_2020_Students/data/shaders/phong.fs
+++ Lab1/ACG_2020_Students/data/shaders/phong.fs
@@ -9,43 +9,53 @@ uniform vec4 u_color;
 uniform sampler2D u_texture;
 uniform vec3 u_light_position;
 uniform vec3 u_light_color;
+uniform float u_has_light;
 
 void main()
 {
-	//here we store the L vector
-	vec3 L;
+	vec4 color;
 
-	//vector from the point to the light
-	L = u_light_position - v_world_position;
+	if (u_has_light == 0) {
+		//here we store the L vector
+		vec3 L;
 
-	//we ignore the light distance for now
-	L = normalize(L);
+		//vector from the point to the light
+		L = u_light_position - v_world_position;
+
+		//we ignore the light distance for now
+		L = normalize(L);
 	
-	//here we can store the total amount of light
-	vec3 light = vec3(0.0);
+		//here we can store the total amount of light
+		vec3 light = vec3(0.0);
 
-	//lets add the ambient light first
-	//light += u_ambient_light;
+		//lets add the ambient light first
+		//light += u_ambient_light;
 
-	//very important to normalize as they come
-	//interpolated so normalization is lost
-	vec3 N = normalize( v_normal );
+		//very important to normalize as they come
+		//interpolated so normalization is lost
+		vec3 N = normalize( v_normal );
 
-	//compute how much is aligned
-	float NdotL = dot(N,L);
+		//compute how much is aligned
+		float NdotL = dot(N,L);
 
-	//light cannot be negative (but the dot product can)
-	NdotL = clamp( NdotL, 0.0, 1.0 );
+		//light cannot be negative (but the dot product can)
+		NdotL = clamp( NdotL, 0.0, 1.0 );
 
-	//store the amount of diffuse light
-	light += NdotL * u_light_color;
+		//store the amount of diffuse light
+		light += NdotL * u_light_color;
 
-	vec2 uv = v_uv;
+		vec2 uv = v_uv;
 
-	vec4 color = u_color * texture2D( u_texture, uv );
+		color = u_color * texture2D( u_texture, uv );
 
-	//apply to final pixel color
-	color.xyz *= light;
+		//apply to final pixel color
+		color.xyz *= light;
+
+	} else {
+		vec2 uv = v_uv;
+
+		color = u_color * texture2D( u_texture, uv );
+	}
 
 	gl_FragColor = color; 
 }
