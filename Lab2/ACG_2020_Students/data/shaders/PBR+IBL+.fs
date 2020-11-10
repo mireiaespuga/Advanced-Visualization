9e6be02adf0c2645dec7d1cbe99da844a369abdf
Mireia Espuga <mireiaespuga@MacBook-MacBook-Pro-de-Mireia.local>
2020-11-10 14:49:16 +0100

    feat: endreçat shader + tonemapping + gamma/degamma

--- Lab2/ACG_2020_Students/data/shaders/PBR+IBL+.fs
+++ Lab2/ACG_2020_Students/data/shaders/PBR+IBL+.fs
@@ -49,6 +49,26 @@ uniform samplerCube u_texture_prem_4;
 #define GAMMA 2.2
 #define INV_GAMMA 0.45
 
+struct SceneVectors{
+	vec3 V;
+	vec3 L;
+	vec3 N;
+	vec3 H;
+	vec3 R;
+	float NoL;
+	float NoV;
+	float NoH;
+	float LoH;
+} sceneVectors;
+
+struct MatProps{
+	float ambient_occlusion;
+	float metalness;
+	float roughness;
+	vec4 emissive;
+	vec4 color;
+} matProps;
+
 mat3 cotangent_frame(vec3 N, vec3 p, vec2 uv){
 	// get edge vectors of the pixel triangle
 	vec3 dp1 = dFdx( p );
@@ -73,6 +93,7 @@ vec3 perturbNormal( vec3 N, vec3 V, vec2 texcoord, vec3 normal_pixel )
 	mat3 TBN = cotangent_frame(N, V, texcoord);
 	return normalize(TBN * normal_pixel);
 }
+
 // Normal Distribution Function --D
 float NDF (	const in float NoH, const in float alpha )
 {
@@ -96,6 +117,12 @@ float Geometry( float NdotV, float NdotL, float roughness)
 	float GL = NdotL / (NdotL * (1.0 - k) + k);
 	float GV = NdotV / (NdotV * (1.0 - k) + k);
 	return GL * GV;
+}
+
+//tonemapping
+vec3 toneMap(vec3 color)
+{
+    return color / (color + vec3(1.0));
 }
 
 // degamma
@@ -129,6 +156,7 @@ vec3 specularDFG( float roughness, vec3 f0, float NoH, float NoV, float NoL, flo
 
 	return spec;
 }
+
 vec3 getReflectionColor(vec3 r, float roughness)
 {
 	float lod = roughness * 5.0;
@@ -152,122 +180,171 @@ vec3 getReflectionColor(vec3 r, float roughness)
 	return color.rgb;
 }
 
-void main()
-{	
-	vec4 color;
-	vec2 uv = v_uv;
-	if (u_has_texture == 1.0) {
-		
-		color = u_color * texture2D( u_color_texture, uv );
-	} else {
-		color = u_color;
-	}
-
-
-	float metalness;
-	float roughness;
-
-	if (u_has_metalness_texture == 1.0){
-		metalness = clamp(texture2D( u_metalness_texture, uv ).x, 0.01, 0.99);
-	} else {
-		metalness = u_metalness;
-	}
-	if (u_has_roughness_texture == 1.0){
-		roughness = clamp(texture2D( u_roughness_texture, uv ).x, 0.01, 0.99);
-	} else {
-		roughness = u_roughness;
-	}
-	vec4 emissive=vec4(0.0);
-	if (u_has_emissive_texture == 1.0){
-		emissive= texture2D( u_emissive_texture, uv );
-	}
-
-	float ambient_light = 1.0;
-	if (u_has_ao_texture == 1.0){
-		ambient_light = clamp(texture2D( u_ao_texture, uv ).x, 0.01, 0.99);
-	}
-
+void setsceneVectors()
+{
 	//vector towards the eye (V)
-	vec3 V = normalize( u_camera_position - v_world_position );
+	sceneVectors.V = normalize( u_camera_position - v_world_position );
 
 	//vector from the point to the light (L)
-	vec3 L =  u_light_position - v_world_position;
-	float light_distance = length(L);
-	L = normalize(L);
+	sceneVectors.L =  u_light_position - v_world_position;
+	sceneVectors.L = normalize(sceneVectors.L);
 
 	//normal vector at the point (N)
-	vec3 N = normalize( v_normal );
+	sceneVectors.N = normalize( v_normal );
 
 	//half vector between V and L (H)
-	vec3 H = normalize( V + L );
+	sceneVectors.H = normalize( sceneVectors.V  + sceneVectors.L );
 
+	//reflection vector R
+	sceneVectors.R = reflect( -sceneVectors.V, sceneVectors.N );
+	sceneVectors.R = normalize(sceneVectors.R);
+}
+
+void setMaterialProps(vec2 uv)
+{
+
+	if (u_has_texture == 1.0) {
+		matProps.color = u_color * texture2D( u_color_texture, uv );
+	} else {
+		matProps.color = u_color;
+	}
 	
-	if (u_has_normal_texture == 1.0){
-		vec3 normal_c = texture2D( u_normal_texture, uv ).xyz;
-		N = perturbNormal( N, V, uv, normal_c );
+	//Color to linear
+	matProps.color.rgb = gamma_to_linear(matProps.color.rgb);
+
+	if (u_has_metalness_texture == 1.0){
+		matProps.metalness = clamp(texture2D( u_metalness_texture, uv ).x, 0.01, 0.99);
+	} else {
+		matProps.metalness = u_metalness;
 	}
 
-	//compute 
-	float NoL = dot(N,L);
-	NoL = clamp( NoL, 0.01, 0.99);
+	if (u_has_roughness_texture == 1.0){
+		matProps.roughness = clamp(texture2D( u_roughness_texture, uv ).x, 0.01, 0.99);
+	} else {
+		matProps.roughness = u_roughness;
+	}
+	
+	if (u_has_emissive_texture == 1.0){
+		matProps.emissive= texture2D( u_emissive_texture, uv );
+	}else{
+		matProps.emissive= vec4(0.0);
+	}
 
-	float NoV = dot(N,V);
-	NoV = clamp( NoV, 0.01, 0.99);
+	//Emissive to linear
+	matProps.emissive.rgb = gamma_to_linear(matProps.emissive.rgb);
 
-	float NoH = dot(N,H);
-	NoH = clamp( NoH, 0.01, 0.99);
+	if (u_has_ao_texture == 1.0){
+		matProps.ambient_occlusion = clamp(texture2D( u_ao_texture, uv ).x, 0.01, 0.99);
+	}else{
+		matProps.ambient_occlusion = 1.0;
+	}
 
-	float LoH = dot(L,H);
-	LoH = clamp( LoH, 0.01, 0.99);
+	if (u_has_normal_texture == 1.0){
+		vec3 normal_c = texture2D( u_normal_texture, uv ).xyz;
+		sceneVectors.N = perturbNormal(sceneVectors.N, sceneVectors.V, uv, normal_c );
+	}
+}
 
-	vec3 R = reflect( -V, N );
-	R = normalize(R);
-
-	//specular F0 (conductors -> base color, dielectrics -> vec3(0.04))
-	vec3 f0 = (1.0 - metalness) * vec3(0.04) + metalness * color.xyz;
-
+vec3 computeDirect(vec3 f0, vec3 diffuseColor)
+{
+	
 	//compute the specular
-	vec3 FSpecular = specularDFG(  roughness, f0, NoH, NoV, NoL, LoH);
-
-	//diffuse cDiff (conductors -> vec3(0.0), dielectrics -> base color)
-	vec3 diffuseColor = (1.0 - metalness) * color.xyz;
+	vec3 FSpecular = specularDFG(  matProps.roughness, f0, sceneVectors.NoH, sceneVectors.NoV, sceneVectors.NoL, sceneVectors.LoH);
 
 	//fLambert
 	vec3 FDiffuse = diffuseColor / PI;
 
 	//diffuse and specular terms from direct lighting
-	vec3 direct = FSpecular + FDiffuse;
+	return FSpecular + FDiffuse;
+}
 
-	//IBL
+vec3 computeIBL(vec3 f0, vec3 diffuseColor)
+{
+	
 	//diffuse IBL
-	vec3 diffuseSample = getReflectionColor ( R, roughness );
+	vec3 diffuseSample = getReflectionColor ( sceneVectors.R, matProps.roughness );
 	vec3 diffuseIBL = diffuseSample * diffuseColor;
 
 	//specular IBL
-	vec3 specularSample = getReflectionColor ( R, roughness );
-	vec4 brdfLUT = texture2D( u_texture_brdfLUT, vec2(roughness, NoV));
+	vec3 specularSample = getReflectionColor ( sceneVectors.R, matProps.roughness );
+	vec4 brdfLUT = texture2D( u_texture_brdfLUT, vec2(matProps.roughness, sceneVectors.NoV));
 	vec3 SpecularBRDF = f0 * brdfLUT.x + brdfLUT.y;
 	vec3 specularIBL = specularSample * SpecularBRDF;
 
 	//diffuse and specular terms from IBL
-	vec3 IBL = diffuseIBL + specularIBL;
-	
+	return diffuseIBL + specularIBL;
+}
+
+vec3 computeLightParams()
+{	
 	//compute a linear attenuation factor
+	float light_distance = length(u_light_position - v_world_position);
 	float att_factor = u_light_maxdist - light_distance;
 	att_factor /= u_light_maxdist;
 	att_factor = max(att_factor, 0.0);
 
-	vec3 light_params = u_light_color * u_light_intensity * att_factor;
+	return u_light_color * u_light_intensity * att_factor;
+}
+
+vec4 getPixelColor()
+{
+
+	//compute 
+	sceneVectors.NoL = dot(sceneVectors.N, sceneVectors.L);
+	sceneVectors.NoL = clamp( sceneVectors.NoL, 0.01, 0.99);
+
+	sceneVectors.NoV = dot(sceneVectors.N, sceneVectors.V);
+	sceneVectors.NoV = clamp( sceneVectors.NoV, 0.01, 0.99);
+	 
+	sceneVectors.NoH = dot(sceneVectors.N, sceneVectors.H);
+	sceneVectors.NoH = clamp( sceneVectors.NoH, 0.01, 0.99);
+
+	sceneVectors.LoH = dot(sceneVectors.L, sceneVectors.H);
+	sceneVectors.LoH = clamp( sceneVectors.LoH, 0.01, 0.99);
+
+	//specular F0 (conductors -> base color, dielectrics -> vec3(0.04))
+	vec3 f0 = (1.0 - matProps.metalness) * vec3(0.04) +  matProps.metalness *  matProps.color.rgb;
+
+	//diffuse cDiff (conductors -> vec3(0.0), dielectrics -> base color)
+	vec3 diffuseColor = (1.0 - matProps.metalness) *  matProps.color.rgb;
+
+	//compte direct lighting
+	vec3 direct = computeDirect(f0, diffuseColor);
+
+	//IBL
+	vec3 IBL = computeIBL(f0, diffuseColor);
+	
+	//Compute light properties
+	vec3 light_params = computeLightParams();
+
 	//modulate light 
-	vec3 light = (direct + IBL) * NoL * light_params;
+	vec3 light = (direct + IBL) * sceneVectors.NoL * light_params;
 
 	//apply to final pixel color
-	color.xyz = light;
-	color = color + emissive;
+	matProps.color.xyz = light;
+
+	return matProps.color + matProps.emissive;
+}
+
+void main()
+{	
+	vec2 uv = v_uv;
+	vec4 p_color;
+
+	//Set Vectors V L H N R
+	setsceneVectors();
+	
+	//set material properties from sampler2D textures
+	setMaterialProps(uv); 
+
+	p_color = getPixelColor();
 
 	//gamma
-	color.xyz = linear_to_gamma(color.xyz);
+	p_color.xyz = linear_to_gamma(p_color.xyz);
 
-	gl_FragColor = color;
+	//tonemapping
+	p_color.xyz = toneMap(p_color.xyz);
+
+	gl_FragColor = p_color;
 
 }
